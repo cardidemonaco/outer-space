913ffc297c96c59b9cd5cad5f852c860b94630b6
Cardi DeMonaco Jr <cardidemonaco@gmail.com>
2023-08-16 10:56:18 -0400

    Created initial Entity Framework files, but have not created the database yet.

---
+++ outer-space.Data/Tables/Galaxy.cs
@@ -0,0 +1,10 @@
+﻿namespace outer_space.Data.Tables
+{
+    public class Galaxy
+    {
+        public int GalaxyID { get; set; }
+        public string GalaxyName { get; set; }
+        public string GalaxyDescription { get; set;}
+
+    }
+}
